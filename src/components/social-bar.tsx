1b84d8b59984747490d2dc294b74a08dac624a79
Lautaro Tejerina <lautit@gmail.com>
2020-04-05 21:55:16 -0300

    fix(): remove breakpoints

--- src/components/social-bar.tsx
+++ src/components/social-bar.tsx
@@ -4,10 +4,6 @@ import { jsx } from 'theme-ui'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from '@emotion/styled'
 
-const breakpoints = [576, 768, 992, 1200]
-
-const mq = breakpoints.map(bp => `@media (min-width: ${bp}px)`)
-
 const SocialContainer = styled.div`
   position: absolute;
   bottom: 5%;
